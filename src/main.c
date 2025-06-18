cc790bfe4373e5247a1168b2714ec36cf6e30e8e
Thalys001 <thalyssouza708@gmail.com>
2025-06-17 22:21:08 -0300

    feat: initial structure with constants and state variables

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// Tamanho da tela
+#define WIDTH 20
+#define HEIGHT 10
+
+// Representações
+#define PLAYER_CHAR "🟦"
+#define OBSTACLE_CHAR "🟥"
+
+// Estado
+int playerX = WIDTH / 5;
+int obstacleY = 0;
+int obstacleX;
+int score = 0;
 
 int main() {
-    printf("Hello, Dodger!\n");
     return 0;
 }
